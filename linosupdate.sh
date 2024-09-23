35125b1413da79ff78cdfd851e5f693c9d903337
marmar24012 <118021360+marmar24012@users.noreply.github.com>
2024-09-23 15:01:39 -0400

    Create linosupdate.sh
    This is a script purpose built to determine:
    
    -Is a Linux Client Virtualized Or Not
    -What Virtualization Platform Is It Running On
    -What Distribution Of Linux is the System
    -What Version Of That Distribution Is Running 
    -What Linux Kernel Version Is Running

---
+++ linosupdate.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+#Prints All The Gathered Specifications 
+print_info() {
+    echo "$1"
+}
+
+########Function To Make Sure Script Is Run by Root#######
+check_root() {
+    if [ "$EUID" -ne 0 ]; then
+        echo "This script must be run as root."
+        exit 1
+    fi
+}
+#Runs The check_root Function
+check_root
+
+#########################################################
+########################FLAGS############################
+# Initialize Flags For Proxmox
+#qemu_found=false
+
+# Initialize Flags For VMWare
+vmware_dmesg_found=false
+
+#Initialize Flags for Azure HCI and Azure Cloud
+azurehci_assettag_found=false
+azurehci_demesg_found=false
+
+#Initialize Flags for Azure Cloud
+azurecloud_assettag_found=false
+
+#Initialize Flags For Oracle Cloud 
+oraclecloud_assettag_found=false
+
+#Initialize Flags For KVM 
+kvm_found=false 
+#########################################################
+##########Check If The System Is Virtualized#############
+if hostnamectl | grep -i vm >/dev/null; then
+    print_info "Virtualization: Yes"
+else
+    print_info "Virtualization: No"
+    print_info "Virtualization Client: None (Bare Metal)"
+    # Output distribution and kernel version before exiting
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        print_info "Linux Distribution: $NAME"
+        print_info "Version: $VERSION"
+    else
+        print_info "Distribution information not found."
+    fi
+    print_info "Kernel Version: $(uname -r)"
+    exit 0
+fi
+#########################################################
+####################FLAG TRIGGERS########################
+# Check for QEMU In The System Manufacturer (dmidecode)
+#if dmidecode -s system-manufacturer | grep -i 'qemu'; then
+#   qemu_found=true
+#fi
+
+# Check 'dmesg' For VMware Related Device Drivers
+if dmesg | grep -i 'Manufacturer: VMware' >/dev/null; then
+    vmware_dmesg_found=true
+fi
+# Check 'dmidecode chassis asset tag' For Azure HCI 
+if dmidecode --string chassis-asset-tag | grep -i 2590-0674-1028-0286-5967-9235-45 >/dev/null; then
+    azurehci_assettag_found=true
+fi
+# Check for hyper-v  
+if dmesg | grep -i 'hyper-v' >/dev/null; then
+    azurehci_demesg_found=true
+fi
+# Check 'dmidecode chassis asset tag' For Azure Cloud  
+if dmidecode --string chassis-asset-tag | grep -i 7783-7084-3265-9085-8269-3286-77 >/dev/null; then
+   azurecloud_assettag_found=true
+fi
+# Check 'dmidecode asset tag' For Oracle Cloud  
+if dmidecode | grep -i oracle >/dev/null; then
+   oraclecloud_assettag_found=true 
+fi
+# Check for KVM In The System Manufacturer (dmidecode)
+if dmidecode -s system-manufacturer | grep -i 'kvm'; then
+    kvm_found=true
+fi
+#########################################################
+############Identify Virtualization Client###############
+if   [ "$qemu_found" = true ]; then
+    print_info "Virtualization Client: Proxmox"
+elif [ "$vmware_dmesg_found" = true ]; then
+    print_info "Virtualization Client: VMware"
+elif [ "$azurehci_assettag_found" = true ] && [ "$azurehci_demesg_found" = true ]; then
+	print_info "Virtualization Client: Azure HCI"
+elif [ "$azurecloud_assettag_found" = true ]; then
+    print_info "Virtualization Client: Azure Cloud"
+elif [ "$oraclecloud_assettag_found" = true ]; then
+    print_info "Virtualization Client: Oracle Cloud"
+elif [ "$kvm_found" = true ]; then
+    print_info "Virtualization Client: OLVM/KVM"
+else
+    print_info "No recognized virtualization client found."
+fi
+#########################################################
+###########Get Linux Distribution and Version############                    
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    print_info "Linux Distribution: $NAME"
+    print_info "Version: $VERSION"
+else
+    print_info "Distribution information not found."
+fi
+#Gets Kernel Version 
+ print_info "Kernel Version: $(uname -r)"
+#########################################################
+#####################Versioning##########################
+# 1.0 Identify Proxmox Clients 
+# 1.1 Identify VMware Clients 
+# 1.2 Identify Baremetal Clients
+# 1.3 Identify Azure HCI Clients 
+# 1.4 Identify Azure Cloud Clients
+# 1.5 Identify Oracle Cloud Clients
+# 1.6 Identify KVM Clients  
